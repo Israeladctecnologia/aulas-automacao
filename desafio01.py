7e2a00ffb1f6ad334e8be752a285c0db5005e322
Israel Adc Tecnologia <israeladctecnologia@gmail.com>
2024-05-18 07:58:52 -0300

    Desafio da criação de pastas com pyouogui

---
+++ desafio01.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+pyautogui.click(x=980,y=359,clicks=1,interval=1,button="right", duration=1)
+pyautogui.moveTo(x=1030, y=624, duration=2)
+time.sleep(1)
+pyautogui.click(x=820, y=350, clicks=1,interval=1,button="left", duration=1)
