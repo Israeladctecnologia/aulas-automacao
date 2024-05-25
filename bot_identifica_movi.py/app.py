300bffb9ed3e28700748095558f26f2bacff43d3
Israel Adc Tecnologia <israeladctecnologia@gmail.com>
2024-05-24 21:05:15 -0300

    bot de wts

--- bot_identifica_movi.py/app.py
+++ bot_identifica_movi.py/app.py
@@ -5,10 +5,10 @@ from time import sleep
 while keyboard.is_pressed('1') == False:
     if pyautogui.pixelMatchesColor(881,575,(0,152,0)):
         pyautogui.press('a')
-        #sleep(0.01)
+        sleep(0.01)
     if pyautogui.pixelMatchesColor(947,575,(255,0,0)):
         pyautogui.press('s')
-        #sleep(0.01)
+        sleep(0.01)
     if pyautogui.pixelMatchesColor(1015,576,(244,244,2)):
         pyautogui.press('j')
-        #sleep(0.01)
+        sleep(0.01)
